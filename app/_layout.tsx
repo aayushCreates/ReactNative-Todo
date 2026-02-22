f03fcc658abc2621ea86d094e90821e04ea0a218
AyushAJS <jangidayush89@gmail.com>
2026-02-22 13:48:14 +0530

    convex backend service is init

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,15 @@
 import { ThemeProvider } from "@/hooks/useTheme";
 import { Stack } from "expo-router";
+import { ConvexProvider, ConvexReactClient } from 'convex/react';
+
+const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
+  unsavedChangesWarning: false
+})
 
 export default function RootLayout() {
   return (
-    <ThemeProvider>
+    <ConvexProvider client={convex}>
+      <ThemeProvider>
       <Stack
         screenOptions={{
           headerShown: false,
@@ -17,5 +23,6 @@ export default function RootLayout() {
         />
       </Stack>
     </ThemeProvider>
+    </ConvexProvider>
   );
 }
